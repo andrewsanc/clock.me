165d3b01de93c935edd48ece6385a531fa0794c2
Andrew Sanchez <druu831@gmail.com>
2023-11-09 11:38:53 -0800

    fix btn className

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ export default function App(props) {
     <>
       <div className='absolute top-0 right-0 m-4'>
         <button
-          class='inline-block rounded border-2 border-white px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:border-primary-accent-100 hover:bg-neutral-500 hover:bg-opacity-10 focus:border-primary-accent-100 focus:outline-none focus:ring-0 active:border-white-accent-200 dark:text-primary-100 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10'
+          className='inline-block rounded border-2 border-white px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-white transition duration-150 ease-in-out hover:border-primary-accent-100 hover:bg-neutral-500 hover:bg-opacity-10 focus:border-primary-accent-100 focus:outline-none focus:ring-0 active:border-white-accent-200 dark:text-primary-100 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10'
           onClick={handleOnSignOutBtn}
         >
           Sign Out
